65b6a5f778a52cc1e00a942cdbe0a1b07ecc3ad2
ykim583 <ykim583@aucklanduni.ac.nz>
2023-08-11 02:09:56 +1200

    Edited sharing icons design (css)
    - Updated CSS of sharing to be user-friendly
    - Fully supporting all five main social networks, and auto generates post url when request made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import {useEffect, useState} from 'react'
 import './App.css'
 import axios from "axios";
-import ShareButton from "./ShareButton.jsx";
 
 function App() {
     const [backendData, setBackendData] = useState([{}])
@@ -18,7 +17,6 @@ function App() {
 
     return (
         <>
-            <ShareButton /> {/* Add the ShareButton component */}
             {backendData.movies && backendData.movies.map((movie, i) => (
                 <div key={i}>
                     <h1>{movie}</h1>

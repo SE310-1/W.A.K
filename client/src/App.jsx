023e76148868aae4dba1ab34e69c6c1e62920363
ejiang25 <ejiang25@gmail.com>
2023-08-16 20:49:04 +1200

    feat: searchbar works as you type

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,25 +8,43 @@ function App() {
   const [textFieldValue, setTextFieldValue] = useState('');
   const [filteredMovies, setFilteredMovies] = useState([]);
 
-    useEffect( () => {
-        const fetchData = async () => {
-            const result = await axios(
-                `${import.meta.env.VITE_BASE_API_URL}/test`,
-            );
-            return result.data;
-        };
-        fetchData().then(r => setBackendData(r));
-    }, []);
-    
-    return (
-        <>
-            {backendData.movies && backendData.movies.map((movie, i) => (
-                <div key={i}>
-                    <h1>{movie}</h1>
-                </div>
-            ))}
-        </>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(`${import.meta.env.VITE_BASE_API_URL}/test`);
+      return result.data;
+    };
+    fetchData().then((r) => setBackendData(r));
+  }, []);
+
+  useEffect(() => {
+    // Filter movies based on textFieldValue
+    const filtered = backendData.movies.filter((movie) =>
+      movie.toLowerCase().includes(textFieldValue.toLowerCase())
+    );
+    setFilteredMovies(filtered);
+  }, [textFieldValue, backendData.movies]);
+
+
+  return (
+    <div>
+      <SearchBar
+        style={{
+          backgroundColor: '#000000',
+          borderRadius: '8px',
+          border: '1px solid #ccc',
+        }}
+        value={textFieldValue}
+        onChange={(newValue) => {
+    setTextFieldValue(newValue);
+  }}
+      />
+      {filteredMovies.map((movie, i) => (
+        <div key={i}>
+          <h1>{movie}</h1>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;

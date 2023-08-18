1277e0282eedc989e0cb5e073a5ae7011d82ad19
ejiang25 <ejiang25@gmail.com>
2023-08-18 22:01:27 +1200

    feat: call tmdb api to fetch 10 pages worth of movies

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import SearchBar from '@mkyy/mui-search-bar';
 
 const Index = () => {
@@ -7,47 +7,64 @@ const Index = () => {
   const [textFieldValue, setTextFieldValue] = useState('');
   const [filteredMovies, setFilteredMovies] = useState([]);
 
+  const API_KEY = '8e66d594db8136414e394600886d8cab'; 
+  const BASE_URL = 'https://api.themoviedb.org/3';
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(`${import.meta.env.VITE_BASE_API_URL}/test`);
-      return result.data;
+      try {
+        const movies = [];
+        for (let page = 1; page <= 100; page++) {
+          const response = await axios.get(
+            `${BASE_URL}/discover/movie?api_key=${API_KEY}&page=${page}`
+          );
+          if (response.status === 200) {
+            movies.push(...response.data.results);
+          } else {
+            console.error('Request failed with status:', response.status);
+          }
+        }
+        setBackendData({ movies });
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
     };
-    fetchData().then((r) => setBackendData(r));
+
+    fetchData();
   }, []);
 
   useEffect(() => {
     // Filter movies based on textFieldValue
     const filtered = backendData.movies.filter((movie) =>
-      movie.toLowerCase().includes(textFieldValue.toLowerCase())
+      movie.title.toLowerCase().includes(textFieldValue.toLowerCase())
     );
     setFilteredMovies(filtered);
   }, [textFieldValue, backendData.movies]);
-
 
   return (
     <div>
       <SearchBar
         style={{
-        backgroundColor: '#FFFFFF',
-        borderRadius: '8px',
-        border: '1px solid #ccc',
-        top: '3.5vh',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        position: 'fixed',
+          backgroundColor: '#FFFFFF',
+          borderRadius: '8px',
+          border: '1px solid #ccc',
+          top: '3.5vh',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          position: 'fixed',
         }}
         value={textFieldValue}
         onChange={(newValue) => {
-    setTextFieldValue(newValue);
-  }}
+          setTextFieldValue(newValue);
+        }}
       />
-      {filteredMovies.map((movie, i) => (
-        <div key={i}>
-          <h1>{movie}</h1>
+      {filteredMovies.map((movie) => (
+        <div key={movie.id}>
+          <h1>{movie.title}</h1>
         </div>
       ))}
     </div>
   );
-}
+};
 
-export default Index
+export default Index;

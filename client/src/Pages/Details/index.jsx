db68ac783741aecf73cb5cf261043bf77ce359f4
Kevin Jin <kevink314159@gmail.com>
2023-08-19 21:27:26 +1200

    feat: Implemented API call to tmdb website to fetch movie data

--- client/src/Pages/Details/index.jsx
+++ client/src/Pages/Details/index.jsx
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css'; 
 import SearchBar from '@mkyy/mui-search-bar';
 
 const MovieDetailsPage = () => {
-    // const [movieData, setMovieData] = useState(null);
-    // useEffect(() => {
-    //     // Replace 'YOUR_TMDB_API_KEY' with your actual TMDB API key
-    //     const apiKey = 'YOUR_TMDB_API_KEY';
-    //     const movieId = 'MOVIE_ID'; // Replace with the actual movie ID
+    const [movieData, setMovieData] = useState(null);
 
-    //     fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             setMovieData(data);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error fetching movie data:', error);
-    //         });
-    // }, []);
+    useEffect(() => {
+        const apiKey = '8e66d594db8136414e394600886d8cab'; 
+        const movieId = 'tt0468569'; 
 
-    // if (!movieData) {
-    //     return <div>Loading...</div>;
-    // }
+        fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
+            .then(response => response.json())
+            .then(data => {
+                setMovieData(data);
+            })
+            .catch(error => {
+                console.error('Error fetching movie data:', error);
+            });
+    }, []);
+
+    if (!movieData) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className="movie-details-container">
             <div className="movie-image">
-                <img src="./movie.jpg" alt="Movie Poster" style={{ maxWidth: '100%', height: '100%' }} />
+                <img
+                    src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+                    alt={movieData.title}
+                    style={{ maxWidth: '100%', height: '100%' }}
+                />
             </div>
             <div className="movie-details">
                 <div className="content">
-                    <div className="movie-name">Movie Name</div>
+                    <div className="movie-name">{movieData.title}</div>
                     <div className="details-section">
                         <h2>Cast</h2>
                         <div className="movie-cast">Actor 1, Actor 2, Actor 3</div>
                     </div>
                     <div className="details-section">
                         <h2>Description</h2>
-                        <div className="movie-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</div>
+                        <div className="movie-description">{movieData.overview}</div>
                     </div>
                     <div className="details-section">
                         <h2>Ratings</h2>
-                        <div className="movie-ratings">IMDb: 8.5/10, Rotten Tomatoes: 90%</div>
+                        <div className="movie-ratings">
+                            IMDb: {movieData.vote_average}/10, Popularity: {movieData.popularity}
+                        </div>
                     </div>
                 </div>
             </div>

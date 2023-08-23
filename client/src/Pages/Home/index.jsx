18ea03e4b77f074396cfda89f299adc75bc4d749
dhruvkanth <dhruv@Dhruvs-MacBook-Pro.local>
2023-08-23 19:53:48 +1200

    feat: The home page shows a list of recommended movies

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,28 +1,55 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { BASE_URL, API_KEY } from "../../../env.js";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import "./style.css";
 
 const Index = () => {
-    const [backendData, setBackendData] = useState([{}])
+
+    const [backendData, setBackendData] = useState({ movies: [] });
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios(
-                `${import.meta.env.VITE_BASE_API_URL}/test`,
-            );
-            return result.data;
+            try {
+                const movies = [];
+                for (let page = 1; page <= 1; page++) {
+                    const response = await axios.get(
+                        `${BASE_URL}/discover/movie?api_key=${API_KEY}&page=${page}`
+                    );
+                    if (response.status === 200) {
+                        movies.push(...response.data.results);
+                        console.log('movies', movies);
+                    } else {
+                        console.error('Request failed with status:', response.status);
+                    }
+                }
+                setBackendData({ movies });
+            } catch (error) {
+                console.error('An error occurred:', error);
+            }
         };
-        fetchData().then(r => setBackendData(r));
-    }, []);
+
+        fetchData();
+    }, []); // Empty dependency array, only fetch data once
 
     return (
         <>
-            {backendData.movies && backendData.movies.map((movie, i) => (
-                <div key={i}>
-                    <h1>{movie}</h1>
-                </div>
-            ))}
+            <Carousel showArrows={false} emulateTouch={true} showStatus={false} showThumbs={false} infiniteLoop={true}>
+                {backendData?.movies.slice(0, 8).map((movie, i) => (
+                    <div className="movie-card" key={i}>
+                        <div className="movie-image" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})` }}>
+                        </div>
+                        <div className="movie-details">
+                            <h1 className="movie-details-title">{movie.title}</h1>
+                            <p className="movie-details-description">{movie.overview}</p>
+                            <button>More Info</button>
+                        </div>
+                    </div>
+                ))}
+            </Carousel>
         </>
-    )
+    );
 }
 
-export default Index
+export default Index;

2d5ef7c0ed48b6624d1ee17599ef2819a7ec86c9
Youngmin Kim <100251600+ykim583@users.noreply.github.com>
2023-08-24 03:35:31 +1200

    Fixed error for share button display
    Previously, share button were kept being displayed every page due to its location at main index.html file. However, it is not only displayed when searched some result

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,70 +1,76 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchBar from '@mkyy/mui-search-bar';
+import ShareButtons from '../../Components/Sharebutton'; // Adjust the relative path to match your project structure
 
 const Index = () => {
-  const [backendData, setBackendData] = useState({ movies: [] });
-  const [textFieldValue, setTextFieldValue] = useState('');
-  const [filteredMovies, setFilteredMovies] = useState([]);
+    const [backendData, setBackendData] = useState({ movies: [] });
+    const [textFieldValue, setTextFieldValue] = useState('');
+    const [filteredMovies, setFilteredMovies] = useState([]);
 
-  const API_KEY = '8e66d594db8136414e394600886d8cab'; 
-  const BASE_URL = 'https://api.themoviedb.org/3';
+    const API_KEY = '8e66d594db8136414e394600886d8cab';
+    const BASE_URL = 'https://api.themoviedb.org/3';
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (textFieldValue.trim() !== '') {
-          const response = await axios.get(
-            `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${textFieldValue}`
-          );
-          if (response.status === 200) {
-            setBackendData({ movies: response.data.results });
-          } else {
-            console.error('Request failed with status:', response.status);
-          }
-        } else {
-          setBackendData({ movies: [] }); // Clear movies when search bar is empty
-        }
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (textFieldValue.trim() !== '') {
+                    const response = await axios.get(
+                        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${textFieldValue}`
+                    );
+                    if (response.status === 200) {
+                        setBackendData({ movies: response.data.results });
+                    } else {
+                        console.error('Request failed with status:', response.status);
+                    }
+                } else {
+                    setBackendData({ movies: [] }); // Clear movies when search bar is empty
+                }
+            } catch (error) {
+                console.error('An error occurred:', error);
+            }
+        };
 
-    fetchData();
-  }, [textFieldValue]);
+        fetchData();
+    }, [textFieldValue]);
 
-  useEffect(() => {
-    // Filter movies based on textFieldValue
-    const filtered = backendData.movies.filter((movie) =>
-      movie.title.toLowerCase().includes(textFieldValue.toLowerCase())
-    );
-    setFilteredMovies(filtered);
-  }, [textFieldValue, backendData.movies]);
+    useEffect(() => {
+        // Filter movies based on textFieldValue
+        const filtered = backendData.movies.filter((movie) =>
+            movie.title.toLowerCase().includes(textFieldValue.toLowerCase())
+        );
+        setFilteredMovies(filtered);
+    }, [textFieldValue, backendData.movies]);
 
-  return (
-    <div>
-      <SearchBar
-        style={{
-          backgroundColor: '#FFFFFF',
-          borderRadius: '8px',
-          border: '1px solid #ccc',
-          top: '3.5vh',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          position: 'fixed',
-        }}
-        value={textFieldValue}
-        onChange={(newValue) => {
-          setTextFieldValue(newValue);
-        }}
-      />
-      {filteredMovies.map((movie) => (
-        <div key={movie.id}>
-          <h1>{movie.title}</h1>
+    return (
+        <div>
+            <SearchBar
+                style={{
+                    backgroundColor: '#FFFFFF',
+                    borderRadius: '8px',
+                    border: '1px solid #ccc',
+                    top: '3.5vh',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    position: 'fixed',
+                }}
+                value={textFieldValue}
+                onChange={(newValue) => {
+                    setTextFieldValue(newValue);
+                }}
+            />
+
+            {/* Conditionally render the ShareButtons component */}
+            {filteredMovies.length > 0 && <ShareButtons />}
+
+            {filteredMovies.map((movie) => (
+                <div key={movie.id}>
+                    <h1>{movie.title}</h1>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Index;
+

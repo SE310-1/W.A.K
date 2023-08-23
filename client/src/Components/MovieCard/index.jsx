742fcf8239b174fcacb8bf4ffb3e60b076241043
ejiang25 <ejiang25@gmail.com>
2023-08-23 22:27:59 +1200

    feat: added movie ratings

--- client/src/Components/MovieCard/index.jsx
+++ client/src/Components/MovieCard/index.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import './style.css'; // Make sure your CSS file path is correct
 
 const MovieCard = ({ movie }) => {
+  // Calculate the number of filled stars based on the rating
+  const rating = movie.vote_average; // Rating is between 0 and 10
+  const filledStars = Math.round((rating / 10) * 5); // Convert to a 5-star scale
+
+  // Create an array of star icons with appropriate classes
+  const starIcons = Array.from({ length: filledStars }, (_, index) => (
+    <span
+      key={index}
+      className={`star-icon ${index < filledStars ? 'filled' : ''}`}
+    >
+      ★
+    </span>
+  ));
+
   return (
     <div className="movie-card">
       <img
         src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
         alt={movie.title}
       />
-      <div className="movie-info"> {/* Updated class name */}
-        <p className="release-year">{movie.release_date.slice(0, 4)}</p> {/* Updated class name */}
-        <h3 className="movie-title">{movie.title}</h3> {/* Updated class name */}
+      <div className="movie-info">
+        <div className="rating">{starIcons}</div>
+        <p className="release-year">{movie.release_date.slice(0, 4)}</p>
+        <h3 className="movie-title">{movie.title}</h3>
       </div>
     </div>
   );
 };
 
 export default MovieCard;
+

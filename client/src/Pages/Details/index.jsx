6f683135bcb0719c9b9608a1f8748cc696d33396
adwaitmane <adwait.mane28@gmail.com>
2023-08-25 03:22:10 +1200

    Finished styling the login and details page

--- client/src/Pages/Details/index.jsx
+++ client/src/Pages/Details/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './styles.css';
 import { useParams } from 'react-router-dom';
-import {apiKey} from '../../../env.js';
+import { apiKey } from '../../../env.js';
 import ShareButtons from '../../Components/Sharebutton'; 
 
 const MovieDetailsPage = () => {
@@ -12,7 +12,6 @@ const MovieDetailsPage = () => {
     const movieId = urlParts[urlParts.length - 1];
 
     useEffect(() => {
-
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
             .then(response => response.json())
             .then(data => {
@@ -32,35 +31,43 @@ const MovieDetailsPage = () => {
         return <div>Loading...</div>;
     }
 
+    const backdropUrl = `https://image.tmdb.org/t/p/original${movieData.backdrop_path}`;
+
     return (
-        <div className="movie-details-container-details">
+        <div className="home-container-details">
+        <div className="background-image-details" style={{ backgroundImage: `url(${backdropUrl})`, backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
+        <div className="overlay"></div>  
+        <div className="page-container">
+
+            <div className="movie-details-container-details">
             <div className="movie-image-details">
-                <img
-                    src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
-                    alt={movieData.title}
-                    style={{ maxWidth: '100%', height: '100%' }}
-                />
-            </div>
-            <div className="movie-details-details">
-                <div className="content">
-                    <div className="movie-name">{movieData.title}</div>
-                    <div className="details-section">
-                        <h2>Cast</h2>
-                        <div className="movie-cast">Actor 1, Actor 2, Actor 3</div>
-                    </div>
-                    <div className="details-section">
-                        <h2>Description</h2>
-                        <div className="movie-description">{movieData.overview}</div>
-                    </div>
-                    <div className="details-section">
-                        <h2>Ratings</h2>
-                        <div className="movie-ratings">
-                            IMDb: {movieData.vote_average}/10, Popularity: {movieData.popularity}
+    <img
+        src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+        alt={movieData.title}
+    />
+</div>
+                <div className="movie-details-details">
+                    <div className="content">
+                        <div className="movie-name">{movieData.title}</div>
+                        <div className="details-section">
+                            <h2>Cast</h2>
+                            <div className="movie-cast">Actor 1, Actor 2, Actor 3</div>
                         </div>
+                        <div className="details-section">
+                            <h2>Description</h2>
+                            <div className="movie-description">{movieData.overview}</div>
+                        </div>
+                        <div className="details-section">
+                            <h2>Ratings</h2>
+                            <div className="movie-ratings">
+                                IMDb: {movieData.vote_average}/10, Popularity: {movieData.popularity}
+                            </div>
+                        </div>
+                        {showShareButtons && <ShareButtons />} {/* Render share buttons */}
                     </div>
-                    {showShareButtons && <ShareButtons />} {/* Render share buttons */}
                 </div>
             </div>
+        </div>
         </div>
     );
 };

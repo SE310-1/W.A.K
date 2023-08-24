2fbaa42f635db98eda679a570f5223855cd45892
Youngmin Kim <100251600+ykim583@users.noreply.github.com>
2023-08-24 16:59:02 +1200

    Fix error for Home index.jss
    - Added Missing braces to actually run and compile

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -6,32 +6,32 @@ import MediaGrid from "../../Components/MediaGrid";
 import './style.css';
 
 const Index = () => {
-  const [backendData, setBackendData] = useState({ movies: [] });
-  const [textFieldValue, setTextFieldValue] = useState("");
-  const [filteredMovies, setFilteredMovies] = useState([]);
+    const [backendData, setBackendData] = useState({ movies: [] });
+    const [textFieldValue, setTextFieldValue] = useState("");
+    const [filteredMovies, setFilteredMovies] = useState([]);
 
-  const API_KEY = "8e66d594db8136414e394600886d8cab";
-  const BASE_URL = "https://api.themoviedb.org/3";
+    const API_KEY = "8e66d594db8136414e394600886d8cab";
+    const BASE_URL = "https://api.themoviedb.org/3";
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (textFieldValue.trim() !== "") {
-          const response = await axios.get(
-            `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${textFieldValue}`
-          );
-          if (response.status === 200) {
-            setBackendData({ movies: response.data.results });
-          } else {
-            console.error("Request failed with status:", response.status);
-          }
-        } else {
-          setBackendData({ movies: [] }); // Clear movies when search bar is empty
-        }
-      } catch (error) {
-        console.error("An error occurred:", error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (textFieldValue.trim() !== "") {
+                    const response = await axios.get(
+                        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${textFieldValue}`
+                    );
+                    if (response.status === 200) {
+                        setBackendData({ movies: response.data.results });
+                    } else {
+                        console.error("Request failed with status:", response.status);
+                    }
+                } else {
+                    setBackendData({ movies: [] }); // Clear movies when search bar is empty
+                }
+            } catch (error) {
+                console.error("An error occurred:", error);
+            }
+        };
 
         fetchData();
     }, [textFieldValue]);
@@ -48,42 +48,25 @@ const Index = () => {
         <div>
             <SearchBar
                 style={{
-                    backgroundColor: '#FFFFFF',
-                    borderRadius: '8px',
-                    border: '1px solid #ccc',
-                    top: '3.5vh',
-                    left: '50%',
-                    transform: 'translateX(-50%)',
-                    position: 'fixed',
+                    backgroundColor: "#FFFFFF",
+                    borderRadius: "8px",
+                    border: "1px solid #ccc",
+                    top: "3.5vh",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    position: "fixed",
                 }}
                 value={textFieldValue}
                 onChange={(newValue) => {
                     setTextFieldValue(newValue);
                 }}
+                placeholder="Search Movie"
             />
-  return (
-    <div>
-      <SearchBar
-        style={{
-          backgroundColor: "#FFFFFF",
-          borderRadius: "8px",
-          border: "1px solid #ccc",
-          top: "3.5vh",
-          left: "50%",
-          transform: "translateX(-50%)",
-          position: "fixed",
-        }}
-        value={textFieldValue}
-        onChange={(newValue) => {
-          setTextFieldValue(newValue);
-        }}
-        placeholder="Search Movie"
-      />
-       <div className="movie-list">
-       <MediaGrid medias={filteredMovies} />
-      </div>
-    </div>
-  );
+            <div className="movie-list">
+                <MediaGrid medias={filteredMovies} />
+            </div>
+        </div>
+    );
 };
 
 export default Index;

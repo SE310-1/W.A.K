4a7814a1b7860a2d180a47cd529be2a60d0cf69a
dhruvkanth <dhruv@Dhruvs-MacBook-Pro.local>
2023-08-27 15:17:27 +1200

    fix: got rid of the repeated stars on the details page

--- client/src/Pages/Details/index.jsx
+++ client/src/Pages/Details/index.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import './styles.css';
 import ReactStars from 'react-stars'
-import { useParams } from 'react-router-dom';
-import { apiKey } from '../../../env.js';
+import {useParams} from 'react-router-dom';
+import {apiKey} from '../../../env.js';
 import ShareButtons from '../../Components/Sharebutton';
 import {useAuthContext} from "../../Hooks/useAuthContext";
 import {useRating} from "../../Hooks/useRating";
@@ -10,22 +10,21 @@ import {useRating} from "../../Hooks/useRating";
 const MovieDetailsPage = () => {
     const [movieData, setMovieData] = useState(null);
     const [showShareButtons, setShowShareButtons] = useState(false);
-    const { id } = useParams();
+    const {id} = useParams();
     const urlParts = id.split('/');
     const movieId = urlParts[urlParts.length - 1];
-    const { user } = useAuthContext()
-    const{rating} = useRating()
+    const {user} = useAuthContext()
+    const {rating} = useRating()
 
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
             .then(response => response.json())
             .then(data => {
                 setMovieData(data);
-                // Simulate the delay before showing the share buttons
                 const timer = setTimeout(() => {
-                    setShowShareButtons(true); // Show the share buttons after the delay
-                }, 500); // Adjust the time in milliseconds as needed
-                return () => clearTimeout(timer); // Clean up the timeout on unmount
+                    setShowShareButtons(true);
+                }, 500);
+                return () => clearTimeout(timer);
             })
             .catch(error => {
                 console.error('Error fetching movie data:', error);
@@ -36,69 +35,56 @@ const MovieDetailsPage = () => {
         return <div>Loading...</div>;
     }
 
-
-
     const backdropUrl = `https://image.tmdb.org/t/p/original${movieData.backdrop_path}`;
 
     const ratingChanged = async (newRating) => {
-       await rating(user.username, newRating, movieData.title)
+        await rating(user.username, newRating, movieData.title)
     }
 
     return (
         <div className="home-container-details">
-        <div className="background-image-details" style={{ backgroundImage: `url(${backdropUrl})`, backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
-        <div className="overlay"></div>  
-        <div className="page-container">
+            <div className="background-image-details" style={{
+                backgroundImage: `url(${backdropUrl})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat"
+            }}></div>
+            <div className="overlay"></div>
+            <div className="page-container">
 
-            <div className="movie-details-container-details">
-            <div className="movie-image-details">
-    <img
-        src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
-        alt={movieData.title}
-    />
-</div>
-                <div className="movie-details-details">
-                    <div className="content">
-                        <div className="movie-name">{movieData.title}</div>
-                        <div className="details-section">
-                            <h2>Cast</h2>
-                            <div className="movie-cast">Actor 1, Actor 2, Actor 3</div>
-                        </div>
-
-                        <div className="details-section">
-                            <h2>Description</h2>
-                            <div className="movie-description">{movieData.overview}</div>
-                        </div>
-                        <div className="details-section">
-                            <h2>Ratings</h2>
-                            <div className="movie-ratings">
-                                IMDb: {movieData.vote_average}/10, Popularity: {movieData.popularity}
+                <div className="movie-details-container-details">
+                    <div className="movie-image-details">
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+                            alt={movieData.title}
+                        />
+                    </div>
+                    <div className="movie-details-details">
+                        <div className="content">
+                            <div className="movie-name">{movieData.title}</div>
+                            <div className="details-section">
+                                <h2>Description</h2>
+                                <div className="movie-description">{movieData.overview}</div>
                             </div>
-                        </div>
-                        {showShareButtons && <ShareButtons />} {/* Render share buttons */}
-
-                        <div>
-                            <h2>Your rating</h2>
-                            <ReactStars
-                                count={5}
-                                onChange={ratingChanged}
-                                size={24}
-                                color2={'#ffd700'}
-                            />
-                        </div>
-                        <div>
-                            <h2>Your rating</h2>
-                            <ReactStars
-                                count={5}
-                                onChange={ratingChanged}
-                                size={24}
-                                color2={'#ffd700'}
-                            />
+                            <div className="details-section">
+                                <h2>Ratings</h2>
+                                <div className="movie-ratings">
+                                    IMDb: {movieData.vote_average}/10, Popularity: {movieData.popularity}
+                                </div>
+                            </div>
+                            {showShareButtons && <ShareButtons/>}
+                            <div>
+                                <h2>Your rating</h2>
+                                <ReactStars
+                                    count={5}
+                                    onChange={ratingChanged}
+                                    size={24}
+                                    color2={'#ffd700'}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 };

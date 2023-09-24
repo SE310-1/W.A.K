ff78b4b5c9afe3530a0f3beed4139b1461f4fff9
vdav02 <100258989+vdav02@users.noreply.github.com>
2023-09-24 15:30:03 +1300

    Placeholder implementation of favourites page

---
+++ client/src/Pages/Favourites/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Favourites = () => {
+    // The actual list component content goes here
+    return (
+        <div>
+            <h1>Favourites</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                {/* Add more list items as needed */}
+            </ul>
+        </div>
+    );
+}
+
+export default Favourites;

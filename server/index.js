b7a821b8b620cacf2d844d5e541f6d81ebc12fd6
Kenny Lam <lamkenny1010@gmail.com>
2023-09-24 13:09:10 +1300

    Enable CORS and create two methods, which are search users and add user as friends.

--- server/index.js
+++ server/index.js
@@ -1,80 +1,91 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const User = require("./src/models/userModel");
 const Rating = require("./src/models/ratingModel");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const userModel = require("./src/models/userModel");
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
-}
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
     next();
 });
 
-app.get('/test', (req, res) => {
-    res.json({"movies": ["The Matrix", "The Matrix Reloaded", "The Matrix Revolutions"]});
+app.get("/test", (req, res) => {
+    res.json({
+        movies: ["The Matrix", "The Matrix Reloaded", "The Matrix Revolutions"],
+    });
 });
 
-app.get('/search', (req, res) => {
-    res.sendFile(__dirname + '../../client/src/Pages/Search/Search.jsx');
+app.get("/search", (req, res) => {
+    res.sendFile(__dirname + "../../client/src/Pages/Search/Search.jsx");
 });
 
-app.post('/login', async (req, res) => {
-    const {username, password} = req.body
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
 
     try {
-        const user = await User.login(username, password)
+        const user = await User.login(username, password);
 
         // create a token
-        const token = createToken(user._id)
+        const token = createToken(user._id);
 
-        res.status(200).json({username, token})
+        res.status(200).json({ username, token });
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message });
     }
 });
 
-app.post('/signup', async (req, res) => {
-    const {email, username, password} = req.body
+app.post("/signup", async (req, res) => {
+    const { email, username, password } = req.body;
 
     try {
-        const user = await User.signup(email, username, password)
-        const token = createToken(user._id)
-        res.status(200).json({email, username, token})
+        const user = await User.signup(email, username, password);
+        const token = createToken(user._id);
+        res.status(200).json({ email, username, token });
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message });
     }
 });
 
-app.post('/rating', async (req, res) => {
-    const {username, rating, movie} = req.body
+app.post("/rating", async (req, res) => {
+    const { username, rating, movie } = req.body;
 
-    try{
-        console.log("WAITIN")
+    try {
+        console.log("WAITIN");
         await Rating.review(username, movie, rating);
-        res.status(200).json({username, movie, rating})
-    }
-    catch(error) {
-        res.status(400).json({error: error.message})
+        res.status(200).json({ username, movie, rating });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 });
 
-app.get('/api/rating/movie/:movieId', async (req, res) => {
+app.get("/api/rating/movie/:movieId", async (req, res) => {
     const { movieId } = req.params;
     const username = req.user.username; // Assuming you have user authentication middleware
 
     try {
         const existingUserRating = await Rating.findOne({ username });
         if (existingUserRating) {
-            const movieRating = existingUserRating.movies.find(item => item.movieId === movieId);
+            const movieRating = existingUserRating.movies.find(
+                (item) => item.movieId === movieId
+            );
             if (movieRating) {
                 res.json({ rating: movieRating.rating });
             } else {
@@ -84,16 +95,45 @@ app.get('/api/rating/movie/:movieId', async (req, res) => {
             res.json({ rating: null }); // User has not rated any movies
         }
     } catch (error) {
-        console.error('Error fetching movie rating:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error("Error fetching movie rating:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
+app.get("/users", async (req, res) => {
+    try {
+        const result = await User.find();
+        res.json(result);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: error.message });
+    }
+});
 
-mongoose.connect(process.env.MONGO_URI)
+app.put("/user/:myid/add-friend/:friendId", async (req, res) => {
+    const userId = req.params.myid; // If you are sending userId in the request body
+    const friendId = req.params.friendId; // Get friendId from URL parameters
+
+    try {
+        const result = await User.findByIdAndUpdate(
+            friendId,
+            { $addToSet: { friendsRequests: userId } } // Update the username field
+        );
+        console.log(result);
+        res.send(result);
+    } catch (error) {
+        console.error("Error updating user:", error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+mongoose
+    .connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(process.env.PORT, () => console.log(`Server IS running` + process.env.PORT));
+        app.listen(process.env.PORT, () =>
+            console.log(`Server IS running ` + process.env.PORT)
+        );
     })
-    .catch(err => {
+    .catch((err) => {
         console.log(err);
-    })
+    });

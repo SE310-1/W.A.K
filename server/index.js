20d15aee082f7a62fb61c76cc20886b97971964d
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2023-09-25 11:42:02 +1300

    Added the API endpoints for the favorites list. These handle the retrieval and manipulation of the favorites list.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express();
 const User = require("./src/models/userModel");
 const Rating = require("./src/models/ratingModel");
 const jwt = require('jsonwebtoken')
+const requireAuth = require('./src/middleware/requireAuth');
 
 const createToken = (_id) => {
     return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
@@ -89,6 +90,68 @@ app.get('/api/rating/movie/:movieId', async (req, res) => {
     }
 });
 
+app.get('/favorites', requireAuth, async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.user._id });
+        if (!user) return res.status(404).json({ error: 'User not found' });
+        res.status(200).json( user.favorites );
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.post('/favorites/add/:movieId', requireAuth, async (req, res) => {
+    const { movieId } = req.params;
+
+    try {
+        const user = await User.findOne({ _id: req.user._id });
+        if (!user) return res.status(404).json({ error: 'User not found' });
+        await user.addFavorite(movieId);
+        res.status(200).json({ message: 'Movie added to favorites' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
+app.delete('/favorites/remove/:movieId', requireAuth, async (req, res) => {
+    const { movieId } = req.params;
+
+    try {
+        const user = await User.findOne({ _id: req.user._id });
+        if (!user) return res.status(404).json({ error: 'User not found' });
+        await user.removeFavorite(movieId);
+        res.status(200).json({ message: 'Movie removed from favorites' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.get('/favorites/isFavorite/:movieId', requireAuth, async (req, res) => {
+    const userId = req.user._id;
+    const { movieId } = req.params;
+    
+    try {
+      const user = await User.findById(userId);
+      
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      
+      const isFavorite = user.favorites.find(favoriteMovieId => String(favoriteMovieId) === String(movieId));
+
+      
+      if (!isFavorite) {
+        return res.json({message : 'Movie not in favourites'});
+      }
+      
+      res.status(200).json({message : 'Movie in favorites'});
+    }
+    catch (error) {
+        console.error('Error fetching favorite movie:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {

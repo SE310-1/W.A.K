68e23c4530903b077770f8d7469baa8e6cef1be3
MRlolface249 <ahop089@aucklanduni.ac.nz>
2023-09-26 23:23:57 +1300

    created setup for typescript support, still need to add types to all variables

--- client/src/Components/MovieCard/index.jsx
+++ client/src/Components/MovieCard/index.jsx
@@ -1,39 +1,39 @@
-import './style.css';
-import {useNavigate} from 'react-router-dom';
+import React from "react";
+import "./style.css";
+import { useNavigate } from "react-router-dom";
 
-const MovieCard = ({movie}) => {
-    const rating = movie.vote_average;
-    const filledStars = Math.round((rating / 10) * 5);
-    const navigate = useNavigate();
+const MovieCard = ({ movie }) => {
+  const rating = movie.vote_average;
+  const filledStars = Math.round((rating / 10) * 5);
+  const navigate = useNavigate();
 
-    // Create an array of star icons with appropriate classes
-    const starIcons = Array.from({length: filledStars}, (_, index) => (
-        <span
-            key={index}
-            className={`star-icon ${index < filledStars ? 'filled' : ''}`}
-        >
+  // Create an array of star icons with appropriate classes
+  const starIcons = Array.from({ length: filledStars }, (_, index) => (
+    <span
+      key={index}
+      className={`star-icon ${index < filledStars ? "filled" : ""}`}
+    >
       ★
     </span>
-    ));
+  ));
 
-    const showDetail = () => {
-        navigate(`/detail/${movie.id}`);
-    };
+  const showDetail = () => {
+    navigate(`/detail/${movie.id}`);
+  };
 
-    return (
-        <div className="movie-card" onClick={showDetail}>
-            <img
-                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                alt={movie.title}
-            />
-            <div className="movie-info">
-                <div className="rating">{starIcons}</div>
-                <p className="release-year">{movie.release_date.slice(0, 4)}</p>
-                <h3 className="movie-title">{movie.title}</h3>
-            </div>
-        </div>
-    );
+  return (
+    <div className="movie-card" onClick={showDetail}>
+      <img
+        src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+        alt={movie.title}
+      />
+      <div className="movie-info">
+        <div className="rating">{starIcons}</div>
+        <p className="release-year">{movie.release_date.slice(0, 4)}</p>
+        <h3 className="movie-title">{movie.title}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
-

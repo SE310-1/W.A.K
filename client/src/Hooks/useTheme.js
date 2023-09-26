8e8721a504e7e576cc69c09ce00257d7d685043d
Luca <leas022@aucklanduni.ac.nz>
2023-09-27 01:01:02 +1300

    Set dummy theme data

--- client/src/Hooks/useTheme.js
+++ client/src/Hooks/useTheme.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import { useFavourites } from "./useFavourites";
 
 export const useTheme = () => {
     const [themeColour, setThemeColour] = useState("#FFFFFF");
     const [themeImage, setThemeImage] = useState("");
+    // const { favourites, loading, error, addFavourite, removeFavourite, isFavourite } = useFavourites();
+
+    setThemeColour("#000000");
+    setThemeImage("https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg");
+
+    return { themeColour, themeImage };
 };

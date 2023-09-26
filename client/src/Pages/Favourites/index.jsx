ef4a6fe262807a14a990c7a5c51ba1eecefcfd9c
MRlolface249 <ahop089@aucklanduni.ac.nz>
2023-09-26 17:37:12 +1300

    added a background image to the favourites page which will later dynamically change based off of the favourites movie list

--- client/src/Pages/Favourites/index.jsx
+++ client/src/Pages/Favourites/index.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
+import FavouritesList from "../../Components/FavouritesList";
+import backgroundImage from "..//Search/img/movies.jpeg";
+import "./style.css";
 
 const Favourites = () => {
-    // The actual list component content goes here
-    return (
-        <div>
-            <h1>Favourites</h1>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                {/* Add more list items as needed */}
-            </ul>
-        </div>
-    );
-}
+  // The actual list component content goes here
+
+  return (
+    <div className="home-container-search">
+      <div
+        className="background-image-favourites"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+      <div className="favourites-list">
+        <FavouritesList />
+      </div>
+    </div>
+  );
+};
 
 export default Favourites;
--- client/src/Pages/Search/Search.jsx
+++ client/src/Pages/Search/Search.jsx
@@ -4,84 +4,89 @@ import SearchBar from "@mkyy/mui-search-bar";
 import MediaGrid from "../../Components/MediaGrid";
 import "./style.css";
 import { apiKey, BASE_URL } from "../../../env.js";
-import backgroundImage from "./img/movies.jpeg"; 
-
+import backgroundImage from "./img/movies.jpeg";
 
 const Search = () => {
-    const [backendData, setBackendData] = useState({ movies: [] });
-    const [textFieldValue, setTextFieldValue] = useState(""); // Start with an empty search query
-    const [filteredMovies, setFilteredMovies] = useState([]);
-    const [isInitialLoad, setIsInitialLoad] = useState(true); // Track the initial load
+  const [backendData, setBackendData] = useState({ movies: [] });
+  const [textFieldValue, setTextFieldValue] = useState(""); // Start with an empty search query
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [isInitialLoad, setIsInitialLoad] = useState(true); // Track the initial load
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const query =
-                    isInitialLoad || textFieldValue.trim() === ""
-                        ? "a"
-                        : textFieldValue.trim();
-                const response = await axios.get(
-                    `${BASE_URL}/search/movie?api_key=${apiKey}&query=${query}`
-                );
-                if (response.status === 200) {
-                    setBackendData({ movies: response.data.results });
-                } else {
-                    console.error("Request failed with status:", response.status);
-                }
-            } catch (error) {
-                console.error("An error occurred:", error);
-            }
-        };
-
-        fetchData();
-    }, [textFieldValue, isInitialLoad]);
-
-    useEffect(() => {
-        // Filter movies based on textFieldValue
-        const filtered = backendData.movies.filter(
-            (movie) =>
-                movie.title.toLowerCase().includes(textFieldValue.toLowerCase())
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const query =
+          isInitialLoad || textFieldValue.trim() === ""
+            ? "a"
+            : textFieldValue.trim();
+        const response = await axios.get(
+          `${BASE_URL}/search/movie?api_key=${apiKey}&query=${query}`
         );
-        setFilteredMovies(filtered);
-    }, [textFieldValue, backendData.movies]);
-
-    useEffect(() => {
-        // After the initial load, set isInitialLoad to false
-        if (isInitialLoad) {
-            setIsInitialLoad(false);
+        if (response.status === 200) {
+          setBackendData({ movies: response.data.results });
+        } else {
+          console.error("Request failed with status:", response.status);
         }
-    }, [isInitialLoad]);
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    };
 
-    return (
-        <div className="home-container-search">
-        <div className="background-image-search" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
-        <div className="overlay-search"></div>  
-        {/* <div className="page"> */}
-            <SearchBar
-                style={{
-                    color: "black",
-                    backgroundColor: "white",
-                    borderRadius: "8px",
-                    border: "1px solid #ccc",
-                    position: "absolute",
-                    top: "20px",
-                    left: "50%",
-                    transform: "translateX(-50%)",
-                    zIndex: "999",
-                }}
-                value={textFieldValue}
-                onChange={(newValue) => {
-                    setTextFieldValue(newValue);
-                }}
-                placeholder="Search Movie"
-            />
-            
-            <div className="movie-list">
-                <MediaGrid medias={filteredMovies} />
-            {/* </div> */}
-        </div>
-        </div>
+    fetchData();
+  }, [textFieldValue, isInitialLoad]);
+
+  useEffect(() => {
+    // Filter movies based on textFieldValue
+    const filtered = backendData.movies.filter((movie) =>
+      movie.title.toLowerCase().includes(textFieldValue.toLowerCase())
     );
+    setFilteredMovies(filtered);
+  }, [textFieldValue, backendData.movies]);
+
+  useEffect(() => {
+    // After the initial load, set isInitialLoad to false
+    if (isInitialLoad) {
+      setIsInitialLoad(false);
+    }
+  }, [isInitialLoad]);
+
+  return (
+    <div className="home-container-search">
+      <div
+        className="background-image-search"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+      <div className="overlay-search"></div>
+      {/* <div className="page"> */}
+      <SearchBar
+        style={{
+          color: "black",
+          backgroundColor: "white",
+          borderRadius: "8px",
+          border: "1px solid #ccc",
+          position: "absolute",
+          top: "20px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: "999",
+        }}
+        value={textFieldValue}
+        onChange={(newValue) => {
+          setTextFieldValue(newValue);
+        }}
+        placeholder="Search Movie"
+      />
+
+      <div className="movie-list">
+        <MediaGrid medias={filteredMovies} />
+        {/* </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Search;

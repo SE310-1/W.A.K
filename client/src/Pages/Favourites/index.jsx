160bd2a9b455b005505743e12220794e7dd11783
vdav02 <100258989+vdav02@users.noreply.github.com>
2023-09-26 14:22:08 +1300

    Update FavouritesPage use FavouritesList component
    The FavouritesList component is currently not displaying on the page, so work needs to be done to identify why. Will not be updating any styling until this issue is identified

--- client/src/Pages/Favourites/index.jsx
+++ client/src/Pages/Favourites/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import FavouritesList from '../../Components/FavouritesList'; // import the FavouritesList component
+import { useAuthContext } from '../../Hooks/useAuthContext'; // import the useAuthContext hook
 
 const Favourites = () => {
-    // The actual list component content goes here
+    const { user } = useAuthContext(); // get the user from the AuthContext
     return (
-        <div>
-            <h1>Favourites</h1>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                {/* Add more list items as needed */}
-            </ul>
+        <div className="favourites-page">
+          <h1>Favourites Page</h1>
+          {user ? ( // check if user is not null
+            <FavouritesList /> // render the FavouritesList component
+          ) : (
+            <p>Please log in to see your favourites list.</p> // show a message asking the user to log in
+          )}
         </div>
-    );
-}
-
-export default Favourites;
+      );
+    };
+    
+    export default Favourites;

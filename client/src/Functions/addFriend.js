ef8fdf774244f0f263c267faa8fee0d7ffd78504
Kenny Lam <lamkenny1010@gmail.com>
2023-09-27 13:21:01 +1300

    Refactor js files.

--- client/src/Functions/addFriend.js
+++ client/src/Functions/addFriend.js
@@ -1,17 +1,31 @@
 import axios from "axios";
 
 /**
+ * Constructs the URL for adding a friend.
+ *
+ * @param {string} myUsername - The username of the user who wants to add a friend.
+ * @param {string} friendUsername - The username of the user to be added as a friend.
+ * @returns {string} The constructed URL.
+ */
+const constructAddFriendUrl = (myUsername, friendUsername) => {
+    const baseUrl = import.meta.env.VITE_BASE_API_URL;
+    return `${baseUrl}/user/${myUsername}/add-friend/${friendUsername}`;
+};
+
+/**
  * Sends a request to add a friend for the given user.
  *
  * @function
  * @param {string} myUsername - The username of the user who wants to add a friend.
  * @param {string} friendUsername - The username of the user to be added as a friend.
+ * @returns {Promise} A promise that resolves when the server request is complete.
  */
 export const addFriend = (myUsername, friendUsername) => {
     // Sending a PUT request to the server to add a friend for the given user.
-    axios.put(
-        `${
-            import.meta.env.VITE_BASE_API_URL
-        }/user/${myUsername}/add-friend/${friendUsername}`
-    );
+    return axios
+        .put(constructAddFriendUrl(myUsername, friendUsername))
+        .catch((error) => {
+            console.error("Error adding friend:", error);
+            // Appropriate error handling can be added here.
+        });
 };

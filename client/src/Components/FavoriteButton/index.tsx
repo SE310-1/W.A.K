0c3c069dfb48509de58331646ce13cb644d7a25d
vdav02 <100258989+vdav02@users.noreply.github.com>
2023-09-27 17:38:35 +1300

    commented FavoriteButton

--- client/src/Components/FavoriteButton/index.tsx
+++ client/src/Components/FavoriteButton/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+// Define the props interface for the FavoriteButton component
 interface FavoriteButtonProps {
   movieId: string;
   username: string;
@@ -12,6 +13,7 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
   username,
   token,
 }) => {
+  // State variables to track whether the movie is a favorite and loading status
   const [isFavorite, setIsFavorite] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -20,9 +22,7 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
     const checkFavorite = async () => {
       try {
         const response = await axios.get(
-          `${
-            import.meta.env.VITE_BASE_API_URL
-          }/${username}/favorites/isFavorite/${movieId}`,
+          `${import.meta.env.VITE_BASE_API_URL}/${username}/favorites/isFavorite/${movieId}`,
           {
             headers: { Authorization: `Bearer ${token}` },
           }
@@ -35,15 +35,15 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
       }
     };
 
+    // Invoke the checkFavorite function when the component mounts or when movieId, username, or token changes
     checkFavorite();
   }, [movieId, username, token]);
 
+  // Function to handle adding the movie to favorites
   const handleAddFavorite = async () => {
     try {
       await axios.post(
-        `${
-          import.meta.env.VITE_BASE_API_URL
-        }/${username}/favorites/add/${movieId}`,
+        `${import.meta.env.VITE_BASE_API_URL}/${username}/favorites/add/${movieId}`,
         {},
         {
           headers: { Authorization: `Bearer ${token}` },
@@ -55,8 +55,10 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
     }
   };
 
+  // If loading, display a disabled button with "Loading..."
   if (isLoading) return <button disabled>Loading...</button>;
 
+  // Render the button to add or display a message if the movie is already a favorite
   return (
     <button onClick={handleAddFavorite} disabled={isFavorite}>
       {isFavorite ? "Movie added to favorites !" : "Add to favorites"}

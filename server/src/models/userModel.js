8e4481088e710d290a3a0d52a2391af760403799
MRlolface249 <ahop089@aucklanduni.ac.nz>
2023-10-18 03:33:20 +1300

    added profile fetch method however it causes a 404 error currently

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -8,117 +8,122 @@ const Schema = mongoose.Schema;
 
 // Creating a new schema for the User with several properties
 const userSchema = new Schema({
-    email: {
+  email: {
+    type: String,
+    required: true,
+    unique: true, // The email should be unique for every user
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true, // The username should be unique for every user
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  favorites: [
+    {
+      movieId: {
+        type: String, // storing the ID of the movie according to TMDB
+        required: true,
+      },
+      addedAt: {
+        type: Date, // date when it was added to favourites
+      },
+      movieTitle: {
         type: String,
         required: true,
-        unique: true, // The email should be unique for every user
+      },
     },
-    username: {
-        type: String,
-        required: true,
-        unique: true, // The username should be unique for every user
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    favorites: [
-        {
-            movieId: {
-                type: String, // storing the ID of the movie according to TMDB
-                required: true,
-            },
-            addedAt: {
-                type: Date, // date when it was added to favourites
-            },
-            movieTitle: {
-                type: String,
-                required: true,
-            },
-        },
-    ],
+  ],
 
-    friends: {
-        type: Array,
-    },
-    friendsRequests: {
-        type: Array,
-    },
-    outgoingRequests: {
-        type: Array,
-    },
+  friends: {
+    type: Array,
+  },
+  friendsRequests: {
+    type: Array,
+  },
+  outgoingRequests: {
+    type: Array,
+  },
+  profilePicture: {
+    type: String,
+  },
 });
 
 // Static method to signup a new user
 userSchema.statics.signup = async function (email, username, password) {
-    // Validation checks
+  // Validation checks
 
-    if (!email || !password) {
-        throw Error("All fields must be filled");
-    }
-    if (!validator.isEmail(email)) {
-        throw Error("Email not valid");
-    }
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email not valid");
+  }
 
-    // Check if email already exists in the database
-    const [exists] = await Promise.all([this.findOne({ email })]);
+  // Check if email already exists in the database
+  const [exists] = await Promise.all([this.findOne({ email })]);
 
-    if (exists) {
-        throw Error("Email already in use");
-    }
+  if (exists) {
+    throw Error("Email already in use");
+  }
 
-    // Encrypt the password before storing it
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
+  // Encrypt the password before storing it
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
-    // Create and return a new user
-    return this.create({ email, username, password: hash });
+  // Create and return a new user
+  return this.create({ email, username, password: hash });
 };
 
 // Static method to login a user
 userSchema.statics.login = async function (username, password) {
-    if (!username || !password) {
-        throw Error("All fields must be filled");
-    }
+  if (!username || !password) {
+    throw Error("All fields must be filled");
+  }
 
-    // Check if the user with the provided username exists
-    const [user] = await Promise.all([this.findOne({ username })]);
+  // Check if the user with the provided username exists
+  const [user] = await Promise.all([this.findOne({ username })]);
 
-    if (!user) {
-        throw Error("Incorrect email");
-    }
+  if (!user) {
+    throw Error("Incorrect email");
+  }
 
-    // Check if the provided password matches the stored password
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-        throw Error("Incorrect password");
-    }
+  // Check if the provided password matches the stored password
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect password");
+  }
 
-    return user;
+  return user;
 };
 
 // Method to add a movie to the user's favorites
 userSchema.methods.addFavorite = async function (movieId, movieTitle) {
-    if (this.favorites.some((fav) => fav.movieId === movieId)) {
-        throw Error("Movie already in favorites");
-    }
-    this.favorites.push({
-        movieId: movieId,
-        movieTitle: movieTitle,
-        addedAt: new Date(),
-    });
-    return this.save();
+  if (this.favorites.some((fav) => fav.movieId === movieId)) {
+    throw Error("Movie already in favorites");
+  }
+  this.favorites.push({
+    movieId: movieId,
+    movieTitle: movieTitle,
+    addedAt: new Date(),
+  });
+  return this.save();
 };
 
 // Method to remove a movie from the user's favorites
 userSchema.methods.removeFavorite = async function (movieId) {
-    const index = this.favorites.findIndex((fav) => fav.movieId === movieId);
-    if (index === -1) {
-        throw Error("Movie not found in favorites");
-    }
-    this.favorites.splice(index, 1);
-    return this.save();
+  const index = this.favorites.findIndex((fav) => fav.movieId === movieId);
+  if (index === -1) {
+    throw Error("Movie not found in favorites");
+  }
+  this.favorites.splice(index, 1);
+  return this.save();
 };
+
+// Set
 
 // Exporting the User model for use in other files
 module.exports = mongoose.model("User", userSchema);
